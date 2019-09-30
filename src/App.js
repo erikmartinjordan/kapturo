3f4efe5298f8386b873f26725e65d4ba443d0ac0
Erik Martín Jordán <31826994+erikmartinjordan@users.noreply.github.com>
2019-09-30 12:44:39 +0700

    Added key shortcut to make screencapture

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+            
+    return (
+    <div className = 'App'>
+        <div className = 'Header-Arrow'></div>
+        <div className = 'Box'>
+            <p>Press ⌘ + ⇧ + 4 to make screencapture</p>
+        </div>
     </div>
-  );
+    );
 }
 
 export default App;
--- src/electron-starter.js
+++ src/electron-starter.js
@@ -1,32 +1,83 @@
 const electron = require('electron');
-// Module to control application life.
 const app = electron.app;
-// Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow;
+const nativeImage = electron.nativeImage;
+const ipcMain = electron.ipcMain;
+const Tray = electron.Tray;
+const globalShortcut = electron.globalShortcut;
 
 const path = require('path');
 const url = require('url');
+var shell = require('shelljs');
+
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
 
-function createWindow() {
-    // Create the browser window.
-    mainWindow = new BrowserWindow({width: 800, height: 600});
+const createTray = () => {
+        
+    // Base 64 icon
+    let base64Icon = `data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABmJLR0QA/wD/AP+gvaeTAAABFElEQVQ4jY3SPU4DQQyGYW+AIuEMEQU3AMRPzoAQ1JRwIJCgj3IBBBegDwG04gZQU0SCioiHAq80QiGbqcYev5/tsSNaDo7wginu0G9jSngXMwxxhhrP6CwrcIlJYW/6PceNSJvSSkR8F/ZrRHxGxE1EPLW2g9PMuF/4VrOSGreL4C284x7dOe/nmC4D99LXxSDvFUaoG2AvP+w6y/4L99KGBzziC4fNnGeYYJxB8+D3FL/CBXaa7C8YFuUPmp7/wFv/9TvF+Rx/txXOwLscySbWCv9BtnP6L5yB/VxP+MBq8TbG1SK+U1XVW0RsR8RJRKxHxEbCVfxu4mxhBUW2TlZS55KMclQ7SwkU7dzmx9Y4bGN+AM3eiKGzw3acAAAAAElFTkSuQmCC`;
     
-    const startUrl = process.env.ELECTRON_START_URL || url.format({
-            pathname: path.join(__dirname, '/../build/index.html'),
-            protocol: 'file:',
-            slashes: true
-        });
+    // Setup the menubar with an icon
+    let icon = nativeImage.createFromDataURL(base64Icon);
+    
+    // Creating icon
+    tray = new Tray(icon);
+    
+    // Ingroirng double clicks to make app faster
+    tray.setIgnoreDoubleClickEvents(true);
+    
+    // Add a click handler so that when the user clicks on the menubar icon, it shows
+    // our popup window
+    tray.on('click', (event) => { mainWindow.isVisible() ? mainWindow.hide() : mainWindow.show() });  
+    
+}
 
+const createWindow = () => {
+    
+    // StartURL
+    const startUrl = process.env.ELECTRON_START_URL || url.format({
+        pathname: path.join(__dirname, '/../build/index.html'),
+        protocol: 'file:',
+        slashes: true
+    });
+    
+    // Create the browser window.
+    mainWindow = new BrowserWindow({
+        alwaysOnTop: true,
+        frame: false,
+        fullscreenable: false,
+        transparent: true,
+        titleBarStyle: 'customButtonsOnHover',
+        show: false,
+        width: 300, 
+        height: 350,
+        webPreferences: {
+            nodeIntegration: false,
+            preload: __dirname + '/preload.js'
+        }
+    });
+    
     // and load the index.html of the app.
     mainWindow.loadURL(startUrl);
-
+    
+    // Highlight icon
+    mainWindow.on('show', () => { tray.setHighlightMode('always') });
+    
+    // Non highlight icon
+    mainWindow.on('hide', () => { tray.setHighlightMode('never') });
+    
+    // Set Position of the window
+    setPosition();
+    
     // Open the DevTools.
-    mainWindow.webContents.openDevTools();
-
+    // mainWindow.webContents.openDevTools();
+    
+    // Blur window when close o loses focus
+    mainWindow.on('blur', () => mainWindow.hide() );
+    
     // Emitted when the window is closed.
     mainWindow.on('closed', function () {
         // Dereference the window object, usually you would store windows
@@ -36,27 +87,61 @@ function createWindow() {
     })
 }
 
+const setPosition = () => {
+    
+    const trayPos = tray.getBounds();
+    const windowPos = mainWindow.getBounds();
+    
+    let x, y = 0;
+    
+    if (process.platform == 'darwin') {
+        x = Math.round(trayPos.x - (trayPos.width / 2) - (windowPos.width / 2));
+        y = Math.round(trayPos.y + trayPos.height);
+    } else {
+        x = Math.round(trayPos.x - (windowPos.width / 2));
+        y = Math.round(trayPos.y + trayPos.height * 10);
+    }
+        
+    mainWindow.setPosition(x, y, false);
+    
+}
+
+// Don't show app in the dock
+app.dock.hide();
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on('ready', () => { 
+    
+    // Initialising tray and window
+    createTray(); 
+    createWindow(); 
+    
+    // Capture keyboard events
+    const ret = globalShortcut.register('Command+Shift+4', () => {
+        shell.exec("screencapture electron_pic.jpg", function(){
+            
+            var storageRef = firebase.storage().ref();
+            storageRef.child('acerca/' + file.name).put('electron_pic.jpg').then( snapshot => console.log('Ok') );
+        });
+    });
+    
+});
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
+app.on('window-all-closed',  () => {
     // On OS X it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== 'darwin') {
-        app.quit()
+        app.quit();
     }
 });
 
-app.on('activate', function () {
+app.on('activate', () => {
     // On OS X it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (mainWindow === null) {
-        createWindow()
+        createWindow();
     }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
